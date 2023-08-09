7d2bc47b3e93d39ef51dcd53c002a6a1e8ee1aa4
Artem Mushket <mushket.2018ru@gmail.com>
2023-08-09 19:46:09 +0300

    void

--- components/pages/home/MainSlider/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="main-slider">
-        <h1 class="title-xxl fw-700">MAIN SLIDER</h1>
-    </div>
-</template>
--- layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <LayoutWrapper>
-        <CustomHeader/>
-        <NuxtPage />
-        <CustomFooter />
-    </LayoutWrapper>
-</template>
-<script lang="ts" setup>
-</script>
